a26276764899268dfd64eba312feeb6900ccabde
sonali <sonalikatara@gmail.com>
2015-03-19 13:06:13 -0700

    trying to fis progresql query problem

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -52,8 +52,7 @@ class ReviewsController < ApplicationController
  #     end 
  
              
-      @books = Review.group(:title).select( " reviews.* , AVG(CAST(rating AS INTEGER)) as average_rating , COUNT(reviews.id) as readers_count") 
-    
+      @books = Review.select("*").merge(Review.group(:title).select( "  AVG(CAST(rating AS INTEGER)) as average_rating , COUNT(reviews.id) as readers_count"))
   #  Review.find(:all, :conditions => ["title in ?", @reviews])
 
     # @books = Review.group(:title).count(:rating)
